b37f2a82bf2e1864099570d1132579e6c422043c
Yan Doroshenko <dorosyan@fel.cvut.cz>
2017-03-18 13:23:58 +0100

    [upd] Controller Controller updated to reflect changes in model

--- coolecnik/app/controllers/Application.scala
+++ coolecnik/app/controllers/Application.scala
@@ -1,13 +1,10 @@
 package controllers
 
-import java.sql.Timestamp
-import java.time.LocalDateTime
-
 import models._
 import org.slf4j.LoggerFactory
 import play.api.libs.json._
 import play.api.mvc._
-import slick.driver.PostgresDriver.api.{Database, TableQuery, _}
+import slick.driver.PostgresDriver.api.{Database, _}
 
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -17,46 +14,25 @@ class Application extends Controller {
 
   val log = LoggerFactory.getLogger(classOf[Application])
 
-  implicit var w = new Writes[User] {
-    override def writes(u: User): JsValue = JsObject(Seq(
-      "id" -> JsNumber(u.id),
-      "login" -> JsString(u.login),
-      "email" -> JsString(u.login),
-      "created" -> Json.toJson(u.created.toLocalDateTime)
-    )
-    )
-  }
-
-  implicit var cw = new Writes[Comment] {
-    override def writes(c: Comment): JsValue = JsObject(Seq(
-      "id" -> JsNumber(c.id)
+  implicit var w = new Writes[Player] {
+    override def writes(p: Player): JsValue = JsObject(Seq(
+      "id" -> JsNumber(BigDecimal(p.id)),
+      "login" -> JsString(p.login),
+      "email" -> JsString(p.login)
     )
     )
   }
 
   def getData = Action.async {
     log.info("Info")
-    val db = Database.forConfig("prod")
+    val db = Database.forConfig("dev")
     log.debug("Debug")
     //new DBUtils(db).createSchema.createTestData(10)
 
     log.warn("Warning")
     log.error("Error")
     log.trace("Trace")
-    db.run(T.users.result) map (s => Ok(Json.toJson(s map (u => Json.toJson(u)))))
-  }
-
-  def gamesForUser(id: Long) = Action.async {
-    val db = Database.forConfig("dev")
-
-    db.run(T.comments.result map (s => Ok(Json.toJson(s map (g => Json.toJson(g))))))
-  }
-
-  def saveUser(id: Long) = Action(parse.json) {
-    request =>
-      //val body = request.body.validate
-
-      Ok
+    db.run(Queries.players.result) map (s => Ok(Json.toJson(s map (u => Json.toJson(u)))))
   }
 }
 
@@ -64,12 +40,13 @@ class DBUtils(db: Database) {
   def createSchema = {
     Await.result(
       db.run(DBIO.seq(
-        /*TableQuery[CommentsTable].schema.drop,
-        TableQuery[TopicsTable].schema.drop,
-        TableQuery[UsersTable].schema.drop,*/
-        TableQuery[UsersTable].schema.create,
-        TableQuery[TopicsTable].schema.create,
-        TableQuery[CommentsTable].schema.create
+        Queries.players.schema.create,
+        Queries.friendList.schema.create,
+        Queries.gameTypes.schema.create,
+        Queries.games.schema.create,
+        Queries.caramboleStrikes.schema.create,
+        Queries.poolStrikeTypes.schema.create,
+        Queries.poolStrikes.schema.create
       )), 500.millis)
     this
   }
@@ -78,7 +55,7 @@ class DBUtils(db: Database) {
     Await.result(
       db.run(DBIO.seq(
         (for (i <- 0 until count) yield
-          T.users += User(login = "user" + i, email = "email" + i + "@user.com", password = "password" + i, salt = i.toString, created = Timestamp.valueOf(LocalDateTime.now()))
+          Queries.players += Player(login = "user" + i, email = "email" + i + "@user.com", passwordHash = "password" + i, firstName = None, lastName = None)
           ): _ *
       )), 500.millis)
   }
