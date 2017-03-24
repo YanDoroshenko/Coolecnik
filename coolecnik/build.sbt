7690fc4a89797b318a685d3a084164efea290de7
Yan Doroshenko <dorosyan@fel.cvut.cz>
2017-03-24 12:07:17 +0100

    [new] Test Added integration test for registration process testing

--- coolecnik/build.sbt
+++ coolecnik/build.sbt
@@ -14,7 +14,8 @@ libraryDependencies ++= Seq(jdbc, cache, ws, specs2 % Test,
   "com.typesafe.slick" %% "slick" % "3.1.1",
   "com.h2database" % "h2" % "1.4.191",
   "org.postgresql" % "postgresql" % "9.4.1212",
-  "com.typesafe.play" %% "play-slick" % "2.0.0")
+  "com.typesafe.play" %% "play-slick" % "2.0.0",
+  "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % "test")
 
 fork in run := true
 
--- coolecnik/test/ApplicationSpec.scala
+++ coolecnik/test/ApplicationSpec.scala
@@ -1,30 +1,130 @@
-import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
+import java.util.UUID
 
+import controllers.Implicits._
+import models._
+import org.scalatest.mockito.MockitoSugar
+import play.api.libs.json.Json
 import play.api.test._
-import play.api.test.Helpers._
+import slick.driver.PostgresDriver.api._
+import slick.driver.PostgresDriver.backend.Database
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 /**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
-@RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
+  * Add your spec here.
+  * You can mock out a whole application including requests, plugins etc.
+  * For more information, consult the wiki.
+  */
+class ApplicationSpec extends PlaySpecification with MockitoSugar {
 
-  "Application" should {
+  "registration" should {
+    "return user if successfully registered" in new WithApplication {
 
-    "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
+      val login = UUID.randomUUID().toString
+
+      val l = Registration(login, "email@e.com", "password", None, None)
+
+      val rq = FakeRequest(
+        POST,
+        "/api/register",
+        headers = FakeHeaders(
+          Seq("Content-type" -> "application/json")
+        ),
+        body =
+          s"""
+             |{
+             |"login" : "$login",
+             |"passwordHash" : "password",
+             |"email": "email@e.com"
+             |}
+          """.stripMargin)
+      val a = route(rq).get
+      status(a) shouldEqual CREATED
+
+      val db = Database.forConfig("dev")
+      val p = Await.result(db.run(
+        Queries.players.filter(_.login === login).result
+      ), 5000.millis)
+      contentAsJson(a) shouldEqual Json.toJson(p)
+      Await.result(db.run(
+        Queries.players.filter(_.id === p.head.id).delete
+      ), 5000.millis)
+      Await.result(db.run(
+        Queries.players.filter(_.login === login).result
+      ), 5000.millis) must be empty
     }
 
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
+    "return BAD_REQUEST for bad request" in new WithApplication() {
+      val rq = FakeRequest(
+        POST,
+        "/api/register",
+        headers = FakeHeaders(
+          Seq("Content-type" -> "application/json")
+        ),
+        body =
+          s"""
+             |{
+             |"passwordHash" : "password",
+             |"email": "email@e.com"
+             |}
+          """.stripMargin)
+      val a = route(rq).get
+      status(a) shouldEqual BAD_REQUEST
 
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      contentAsString(a) shouldEqual "Request can't be deserialized"
+    }
+
+    "return NOT_ACCEPTABLE for existing user" in new WithApplication {
+
+      val login = UUID.randomUUID().toString
+
+      val l = Registration(login, "email@e.com", "password", None, None)
+
+      val rq = FakeRequest(
+        POST,
+        "/api/register",
+        headers = FakeHeaders(
+          Seq("Content-type" -> "application/json")
+        ),
+        body =
+          s"""
+             |{
+             |"login" : "$login",
+             |"passwordHash" : "password",
+             |"email": "email@e.com"
+             |}
+          """.stripMargin)
+      val a = route(rq).get
+      status(a) shouldEqual CREATED
+
+      val db = Database.forConfig("dev")
+      val p = Await.result(db.run(
+        Queries.players.filter(_.login === login).result
+      ), 5000.millis)
+
+      contentAsJson(a) shouldEqual Json.toJson(p)
+
+      val b = route(rq).get
+      status(b) shouldEqual NOT_ACCEPTABLE
+      contentAsString(b) must contain("ERROR: duplicate key value violates unique constraint")
+
+      Await.result(db.run(
+        Queries.players.filter(_.id === p.head.id).delete
+      ), 5000.millis)
+      Await.result(db.run(
+        Queries.players.filter(_.login === login).result
+      ), 5000.millis) must be empty
+    }
+
+    "clean up after test" in new WithApplication {
+      val db = Database.forConfig("dev")
+      Await.result(db.run(
+        sql"""SELECT setval('t_player_id_seq', (
+          SELECT MAX(id)
+          FROM t_player))"""
+          .as[String]
+      ), 5000.millis)
     }
   }
 }
