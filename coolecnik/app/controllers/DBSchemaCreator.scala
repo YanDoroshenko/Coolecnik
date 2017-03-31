387fc6350e3ad47e7daf8a8588c3d1cc40e9c1c1
Yan Doroshenko <dorosyan@fel.cvut.cz>
2017-03-31 17:22:31 +0200

    [new] Unique constraints

--- coolecnik/app/controllers/DBSchemaCreator.scala
+++ coolecnik/app/controllers/DBSchemaCreator.scala
@@ -14,16 +14,30 @@ class DBSchemaCreator extends Controller {
   private val db = Database.forConfig("prod")
 
   def createSchema: Action[AnyContent] = Action.async {
-    db.run(DBIO.seq(
-      Queries.tournamentTypes.schema.create,
-      Queries.tournaments.schema.create,
-      Queries.players.schema.create,
-      Queries.friendList.schema.create,
-      Queries.gameTypes.schema.create,
-      Queries.games.schema.create,
-      Queries.strikeTypes.schema.create,
-      Queries.strikes.schema.create)
-    )
+    createTables
+      .flatMap(_ => createConstraints)
       .map(_ => Ok)
   }
+
+  private def createTables = db.run(DBIO.seq(
+    Queries.tournamentTypes.schema.create,
+    Queries.tournaments.schema.create,
+    Queries.players.schema.create,
+    Queries.friendList.schema.create,
+    Queries.gameTypes.schema.create,
+    Queries.games.schema.create,
+    Queries.strikeTypes.schema.create,
+    Queries.strikes.schema.create)
+  )
+
+  private def createConstraints = db.run(DBIO.seq(
+    sqlu"""ALTER TABLE t_player ADD CONSTRAINT player_login UNIQUE (login);""",
+    sqlu"""ALTER TABLE t_player ADD CONSTRAINT player_email UNIQUE (email);""",
+    sqlu"""ALTER TABLE t_game_type ADD CONSTRAINT game_type_title UNIQUE (title);""",
+    sqlu"""ALTER TABLE t_strike_type ADD CONSTRAINT strike_type_title UNIQUE (title);""",
+    sqlu"""ALTER TABLE t_tournament_type ADD CONSTRAINT tournament_type_title UNIQUE (title);""",
+    sqlu"""ALTER TABLE t_tournament ADD CONSTRAINT tournament_title UNIQUE (title);""",
+    sqlu"""ALTER TABLE t_game ADD CONSTRAINT game_players UNIQUE (player1, player2, beginning);"""
+  )
+  )
 }
