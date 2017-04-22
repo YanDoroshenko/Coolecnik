3983e5fed344767a7be27e8ab453ac2f11732afe
Yan Doroshenko <dorosyan@fel.cvut.cz>
2017-04-23 00:46:14 +0200

    [upd] Better error handling

--- coolecnik/app/controllers/GameController.scala
+++ coolecnik/app/controllers/GameController.scala
@@ -13,6 +13,7 @@ import util.JsonSerializers._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
 
 /**
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 30.03.2017.
@@ -24,56 +25,66 @@ class GameController extends Controller {
 
   def newGameType: Action[JsValue] = Action.async(parse.json) {
     rq => {
-      log.info("New game type:\n" + rq.body)
-      Json.fromJson[NewGameType](rq.body).asOpt match {
-        case Some(g) =>
-          db.run(
-            gameTypes.map(
-              g_ => {
-                (g_.title, g_.description)
-              }) +=
-              (g.title, g.description)
-          )
-            .recover {
-              case e: PSQLException => e
-            }
-            .flatMap {
-              case e: PSQLException => Future(Conflict(e.getMessage))
-              case _ =>
-                db.run(gameTypes.filter(
-                  g_ => g_.title === g.title)
-                  .result)
-                  .map(gs => Created(gs.head))
-            }
-        case None => Future(BadRequest("Request can't be deserialized"))
+      Try {
+        log.info("New game type:\n" + rq.body)
+        Json.fromJson[NewGameType](rq.body).asOpt match {
+          case Some(g) =>
+            db.run(
+              gameTypes.map(
+                g_ => {
+                  (g_.title, g_.description)
+                }) +=
+                (g.title, g.description)
+            )
+              .recover {
+                case e: PSQLException => e
+              }
+              .flatMap {
+                case e: PSQLException => Future(Conflict(e.getMessage))
+                case _ =>
+                  db.run(gameTypes.filter(
+                    g_ => g_.title === g.title)
+                    .result)
+                    .map(gs => Created(gs.head))
+              }
+          case None => Future(BadRequest("Request can't be deserialized"))
+        }
+      } match {
+        case Success(v) => v
+        case Failure(e) => Future(BadRequest(e.getMessage))
       }
     }
   }
 
   def newGame: Action[JsValue] = Action.async(parse.json) {
     rq => {
-      log.info("New game:\n" + rq.body)
-      Json.fromJson[NewGame](rq.body).asOpt match {
-        case Some(g) =>
-          db.run(
-            games.map(
-              g_ => {
-                (g_.gameType, g_.player1, g_.player2, g_.beginning, g_.tournament, g_.rounds, g_.carambolesToWin)
-              }) +=
-              (g.gameType, g.player1, g.player2, Some(g.beginning), g.tournament, g.rounds, g.carambolesToWin)
-          )
-            .recover {
-              case e: PSQLException => e
-            }
-            .flatMap {
-              case e: PSQLException => Future(Conflict(e.getMessage))
-              case _ =>
-                db.run(games.filter(
-                  g_ => g_.player1 === g.player1 && g_.player2 === g.player2 && g_.beginning === g.beginning)
-                  .result)
-                  .map(gs => Created(gs.head))
-            }
-        case None => Future(BadRequest("Request can't be deserialized"))
+      Try {
+        log.info("New game:\n" + rq.body)
+        Json.fromJson[NewGame](rq.body).asOpt match {
+          case Some(g) =>
+            db.run(
+              games.map(
+                g_ => {
+                  (g_.gameType, g_.player1, g_.player2, g_.beginning, g_.tournament, g_.rounds, g_.carambolesToWin)
+                }) +=
+                (g.gameType, g.player1, g.player2, Some(g.beginning), g.tournament, g.rounds, g.carambolesToWin)
+            )
+              .recover {
+                case e: PSQLException => e
+              }
+              .flatMap {
+                case e: PSQLException => Future(Conflict(e.getMessage))
+                case _ =>
+                  db.run(games.filter(
+                    g_ => g_.player1 === g.player1 && g_.player2 === g.player2 && g_.beginning === g.beginning)
+                    .result)
+                    .map(gs => Created(gs.head))
+              }
+          case None => Future(BadRequest("Request can't be deserialized"))
+        }
+      } match {
+        case Success(v) => v
+        case Failure(e) => Future(BadRequest(e.getMessage))
       }
     }
   }
