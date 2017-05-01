9d9ce7b43188e5e62ae0be92537327fe4b333fb4
Erik Lehocký <eriklehocky258@gmail.com>
2017-05-01 17:06:22 +0200

    git does not work

--- coolecnik/public/js/statistics.js
+++ coolecnik/public/js/statistics.js
@@ -2,7 +2,7 @@
  * Created by Erik on 17. 4. 2017.
  */
 
-
+//table switch
 document.getElementById('gameType').addEventListener('change', function () {
     var style = this.value == 0 ? 'block' : 'none';
     document.getElementById('bothGames').style.display = style
@@ -11,4 +11,28 @@ document.getElementById('gameType').addEventListener('change', function () {
     var style = this.value == 2 ? 'block' : 'none';
     document.getElementById('karambolGame').style.display = style
 
+});
+
+//opponents fill selectBox
+document.getElementById("playersStatistics").ready(function (){
+    var endpoint = "/api/players/" + getCookie("myId") + "/opponents";
+
+    $.ajax(endpoint, {
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        statusCode: {
+            200: function (response) {
+                var str1 = "<button class='dropdown-item friendBtns' type='button' value='";
+                var str2 = "'>";
+                var str3 = "</button>";
+                var btnArrHtml = [];
+
+                response.forEach(function (item, i, response) {
+                    btnArrHtml.push(str1 + item.id + str2 + item.login + str3);
+                    $("#friendList").html($("#friendList").html() + str1 + item.id + str2 + item.login + str3);
+                });
+
+                }
+            }
+    });
 });
