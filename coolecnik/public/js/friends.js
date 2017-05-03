d24fe6cc6d891a1d8f55c7a1670fdf0230dca5d7
Michal Habel <habelmic@fel.cvut.cz>
2017-05-03 14:06:34 +0200

    Basic friends preparation

---
+++ coolecnik/public/js/friends.js
@@ -0,0 +1,25 @@
+document.getElementById("addFriend").addEventListener("click", function (event) {
+
+    var vName = $('#friendName').val();
+
+    var obj = {
+        "name": vName
+    };
+
+    $.ajax("api/friends", {
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(obj),
+        statusCode: {
+            201: function (response) {
+                                console.log("201 CREATED");
+            },
+            400: function (response) {
+                                console.log("400 BAD REQUEST");
+            },
+            409: function (response) {
+                                console.log("409 CONFLICT");
+            }
+        }
+    });
+}
