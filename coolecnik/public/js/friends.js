24ed11709e734ebf453ba079cf40bc081c679306
Michal Habel <habelmic@fel.cvut.cz>
2017-05-14 19:49:04 +0200

    [upd] Finished friendlist
    Friendlist should be fully functional. Adding and removing works as intended. Some old entries already in DB might have only one-directional connection but new entries should work as they should.

--- coolecnik/public/js/friends.js
+++ coolecnik/public/js/friends.js
@@ -1,21 +1,40 @@
+// thanks to  https://github.com/garycourt/murmurhash-js
+function hash(str, asString, seed) {
+    var i, l,
+        hval = (seed === undefined) ? 0x811c9dc5 : seed;
+
+    for (i = 0, l = str.length; i < l; i++) {
+        hval ^= str.charCodeAt(i);
+        hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+    }
+    if (asString) {
+        // Convert to 8 digit hex string
+        return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+    }
+    return hval >>> 0;
+}
+
+getFriends();
+
 console.log("My ID: " + getCookie("myId"));
 
-var friendPoint = "api/players/" + getCookie("myId") + "/friends";
+function getFriends() {
+    var friendPoint = "api/players/" + getCookie("myId") + "/friends";
+    $.ajax(friendPoint, {
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        statusCode: {
+            200: function (response) {
+                parseFriends(response);
+                console.log("200 OK");
+            },
+            404: function (response) {
+                console.log("friends 404 NOT FOUND");
+            }
 
-$.ajax(friendPoint, {
-    type: "GET",
-    contentType: "application/json; charset=utf-8",
-    statusCode: {
-        200: function (response) {
-            parseFriends(response);
-            console.log("200 OK");
-        },
-        404: function (response) {
-            console.log("friends 404 NOT FOUND");
         }
-
-    }
-});
+    });
+}
 
 function parseFriends(friends) {
     document.getElementById("friendList").innerHTML = "";
@@ -28,12 +47,13 @@ function parseFriends(friends) {
     }
 }
 
-function removeFriend(friendId) {
-    $.ajax("api/players/" + getCookie("myId") + "/unfriend/" + friendId, {
+function secondDirectionRemove(friendId) {
+    $.ajax("api/players/" + friendId + "/unfriend/" + getCookie("myId"), {
         type: "DELETE",
         contentType: "application/json; charset=utf-8",
         statusCode: {
             204: function (response) {
+                getFriends();
                 console.log("204 NO CONTENT");
             },
             404: function (response) {
@@ -44,19 +64,60 @@ function removeFriend(friendId) {
 
 }
 
-function addFriend(name) {
-    var obj= {
-        playerId: getCookie("myId"),
-        friendId: 63 //TODO CANNOT GET ID FROM NAME
+function removeFriend(friendId) {
+    $.ajax("api/players/" + getCookie("myId") + "/unfriend/" + friendId, {
+        type: "DELETE",
+        contentType: "application/json; charset=utf-8",
+        statusCode: {
+            204: function (response) {
+                secondDirectionRemove(friendId);
+                console.log("204 NO CONTENT");
+            },
+            404: function (response) {
+                console.log("404 NOT FOUND");
+            }
+        }
+    });
+
+}
+
+function checkFriend() {
+    var vLogin = $('#friendName').val();
+    var vPassHash = hash($('#friendPassword').val(), true, $('#friendPassword').val().length).toString();
+
+    var obj = {
+        "login": vLogin,
+        "passwordHash": vPassHash
     };
 
-    $.ajax("api/players/" + getCookie("myId") + "/befriend/63", { //TODO CANNOT GET ID FOR ENDPOINT
+    $.ajax("api/login", {
         type: "POST",
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify(obj),
         statusCode: {
+            202: function (response) {
+                console.log("202 ACCEPTED");
+                addFriend(response.id);
+            },
+            400: function (response) {
+                $("#friendSpan").text("Can't deserialize JSON.");
+                console.log("400 BAD REQUEST");
+            },
+            401: function (response) {
+                $("#friendSpan").text("Špatný login nebo heslo. Zkuste ještě jednou.");
+                console.log("401 UNAUTHORIZED");
+            }
+        }
+    });
+}
+
+function secondDirectionAdd(friendId) {
+    $.ajax("api/players/" + friendId + "/befriend/" + getCookie("myId"), {
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        statusCode: {
             201: function (response) {
-                parseFriends(response);
+                $("#friendSpan").text("Přítel úspěšně přidán.");
                 console.log("201 CREATED");
             },
             404: function (response) {
@@ -68,16 +129,38 @@ function addFriend(name) {
 
         }
     });
-
 }
 
-document.getElementById("addFriend").addEventListener("click", function (event) {
-    addFriend(document.getElementById("friendName").value);
-});
+function addFriend(friendId) {
+    $.ajax("api/players/" + getCookie("myId") + "/befriend/" + friendId, {
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        statusCode: {
+            201: function (response) {
+                secondDirectionAdd(friendId);
+                parseFriends(response);
+                $('#addFriendModal').modal('hide');
+                console.log("201 CREATED");
+            },
+            404: function (response) {
+                console.log("404 NOT FOUND");
+            },
+            409: function (response) {
+                console.log("409 CONFLICT");
+            }
 
-var removeBtn = document.getElementsByClassName("removeBtn"); //TODO Doesn't work, probably because of Bootstrap
-for (var i = 0; i < removeBtn.length; i++) {
-    removeBtn[i].addEventListener('click', function (event) {
-        removeFriend($(this).attr('id'));
+        }
     });
 }
+
+// Event Listeners
+document.getElementById("addFriend").addEventListener("click", function (event) {
+    checkFriend();
+});
+
+document.getElementById("friendList").addEventListener("click", function(event) {
+    var target = event.target;
+    if (target.classList.contains("removeBtn")) {
+        removeFriend(target.id);
+    }
+});
