49f27c3687ff99c1155a4ce5804d2bb54ec8e801
Yan Doroshenko <dorosyan@fel.cvut.cz>
2017-05-18 10:48:27 +0200

    [upd] Updated data creation queries

--- coolecnik/app/controllers/DBController.scala
+++ coolecnik/app/controllers/DBController.scala
@@ -58,16 +58,16 @@ class DBController extends Controller {
   private def createStrikeTypes = {
     db.run(
       strikeTypes ++= Seq(
-        StrikeType(1, true, 1, "correct_8pool", Some("player shot the correct ball, same player continues"), false),
-        StrikeType(2, false, 1, "wront_shot_8pool", Some("'player shot, but ball did not go to pocket, other players turn"), false),
+        StrikeType(1, true, 1, "correct_pool", Some("player shot the correct ball, same player continues"), false),
+        StrikeType(2, false, 1, "wrong_shot_pool", Some("'player shot, but ball did not go to pocket, other players turn"), false),
         StrikeType(3, false, 1, "foul_with_white", Some("cue(white) ball scratch or off the table"), false),
         StrikeType(4, false, 1, "foul_with_others_ball", Some("foul_with_others_ball', 'in those games which require the first object ball struck to be a particular ball or one of a group of balls, it is a foul for the cue ball to first contact any other ball"), false),
         StrikeType(5, false, 1, "foul_other", Some("other kind of foul"), false),
         StrikeType(6, true, 1, "game_end_correctly", Some("direct end of the game, win of a player who shot all of his balls to the correct pocket and ball 8 too, PLAYER WON"), true),
-        StrikeType(7, false, 1, "8ball_foul_at_racking", Some("direct end of the game, player who pockets the eight ball and fouls, PLAYER LOST"), true),
-        StrikeType(8, false, 1, "8ball_racked_too_early", Some("direct end of the game, player pockets the eight ball before his group is cleared, PLAYER LOST"), true),
-        StrikeType(9, false, 1, "8ball_to_wrong_hole", Some("direct end of the game, player pockets the eight ball in an uncalled pocket, PLAYER LOST"), true),
-        StrikeType(10, false, 1, "8ball_out_of_table", Some("direct end of the game, player drives the eight ball off the table, PLAYER LOST"), true),
+        StrikeType(7, false, 1, "eightball_foul_at_racking", Some("direct end of the game, player who pockets the eight ball and fouls, PLAYER LOST"), true),
+        StrikeType(8, false, 1, "eightball_racked_too_early", Some("direct end of the game, player pockets the eight ball before his group is cleared, PLAYER LOST"), true),
+        StrikeType(9, false, 1, "eightball_to_wrong_hole", Some("direct end of the game, player pockets the eight ball in an uncalled pocket, PLAYER LOST"), true),
+        StrikeType(10, false, 1, "eightball_out_of_table", Some("direct end of the game, player drives the eight ball off the table, PLAYER LOST"), true),
 
         StrikeType(11, true, 2, "correct_carambole", Some("correct carambole, player continues"), false),
         StrikeType(12, false, 2, "foul_carambole", Some("foul at shot, other players turn"), false)
